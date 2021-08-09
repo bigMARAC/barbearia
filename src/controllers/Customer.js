f908eaeea682aa0b7948cbce66ef6ad4143946af
bigMARAC <marcos.length@gmail.com>
2021-08-09 20:23:06 -0300

    create customer auth route

--- src/controllers/Customer.js
+++ src/controllers/Customer.js
@@ -25,6 +25,30 @@ module.exports = {
     }
   },
   async auth(req, res) {
+    try {
+      const { username, password } = req.body
+      if (!username || !password) {
+        return res.status(400).json({ error: true, message: 'Campos Inválidos.' })
+      }
 
+      const user = await Customer.findOne({
+        where: { username }
+      })
+
+      if (!user) {
+        return res.status(404).json({ error: true, message: 'Nome de usuário inválido.' })
+      }
+      
+      if (user.password != password) {
+        return res.status(401).json({ error: true, message: 'Senha inválida.' })
+      }
+
+      return res.status(200).json({ message: 'Autenticado com sucesso.' })
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: 'Ocorreu um erro ao tentar realizar a operação, \npor favor tente novamente mais tarde.'
+      })
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ const Customer = require('./controllers/Customer')
 routes.get('/', (req, res) => res.send('Barbearia MKO'))
 routes.post('/code/generate', Code.generate)
 routes.post('/customer/store', Customer.store)
+routes.post('/customer/auth', Customer.auth)
 
 module.exports = routes
