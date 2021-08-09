e124487826c8474590f81cdea633eaeaf6a27d7b
bigMARAC <marcos.length@gmail.com>
2021-08-09 17:31:34 -0300

    create store customer method

--- src/controllers/Customer.js
+++ src/controllers/Customer.js
@@ -2,7 +2,27 @@ const Customer = require('../database/models/Customer')
 
 module.exports = {
   async store(req, res) {
+    try {
+      const { name, username, password } = req.body
+      if (!name || !username || !password) {
+        return res.status(400).json({ error: true, message: 'Campos Inválidos.' })
+      }
 
+      const user = await Customer.create({ name, username, password })
+      return res.status(200).json({ message: 'Usuário cadastrado com sucesso.', user })
+    } catch (err) {
+      const type = err.errors[0]
+      if (type.validatorKey == 'not_unique') {
+        return res.status(400).json({
+          error: true,
+          message: `O nome de usuário "${type.value}" já está em uso.`
+        })
+      }
+      return res.status(500).json({
+        error: true,
+        message: 'Ocorreu um erro ao tentar realizar a operação, \npor favor tente novamente mais tarde.'
+      })
+    }
   },
   async auth(req, res) {
 
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const routes = express.Router()
 
 const Code = require('./controllers/Code')
+const Customer = require('./controllers/Customer')
 
 routes.get('/', (req, res) => res.send('Barbearia MKO'))
 routes.post('/code/generate', Code.generate)
+routes.post('/customer/store', Customer.store)
 
 module.exports = routes
