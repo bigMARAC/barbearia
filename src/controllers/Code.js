1a06a9dc570f8862dd024a8dd2451b3354d10c9e
bigMARAC <marcos.length@gmail.com>
2021-08-09 23:45:02 -0300

    update code generate route

--- src/controllers/Code.js
+++ src/controllers/Code.js
@@ -3,13 +3,24 @@ const randomstring = require('randomstring')
 
 module.exports = {
   async generate(req, res) {
+    const { customer_id } = req.params
+    if (!customer_id) {
+      return res.status(400).json({
+        error: true,
+        message: 'ID de cliente inválido'
+      })
+    }
     const code = randomstring.generate({
       length: 7,
       readable: true,
       charset: 'alphanumeric',
       capitalization: 'uppercase'
     })
-    res.status(200).json({code})
+    await Code.create({
+      customer_id,
+      content: code
+    })
+    res.status(200).json({ code })
   },
   async revoke(req, res) {
 
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const AdminMiddleware = require('./middlewares/Admin')
 
 routes.get('/', (req, res) => res.send('Barbearia MKO'))
 routes.post('/barber/auth', Barber.auth)
-routes.post('/code/generate/:customer_id', AdminMiddleware, Code.generate)
+routes.get('/code/generate/:customer_id', AdminMiddleware, Code.generate)
 routes.post('/customer/store', Customer.store)
 routes.post('/customer/auth', Customer.auth)
 
