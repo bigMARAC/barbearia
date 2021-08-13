44b8fd198d7785123ac4953ddf5c18ab3428800a
Marcos Emanuel <marcos.length@gmail.com>
2021-08-12 23:34:15 -0300

    add bcrypt to Customers Controller

--- src/controllers/Customer.js
+++ src/controllers/Customer.js
@@ -1,5 +1,6 @@
 const Customer = require('../database/models/Customer')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
 const secret = process.env.APP_SECRET
 
@@ -11,32 +12,45 @@ module.exports = {
         return res.status(400).json({ error: true, message: 'Campos Inválidos.' })
       }
 
-      const seconds = 43200
-      
+      const saltRounds = 12
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) return res.status(500)
+        try {
+          const user = await Customer.create({ name, username, password: hash })
+          const seconds = 43200
+          const token = jwt.sign(
+            {
+              id: user.id,
+              timestamp: Date.now(),
+              admin: false
+            },
+            secret,
+            { expiresIn: seconds }
+          )
 
-      const user = await Customer.create({ name, username, password })
+          user.token = token
+          await user.save()
 
-      const token = jwt.sign(
-        {
-          id: user.id,
-          timestamp: Date.now(),
-          admin: false
-        },
-        secret,
-        { expiresIn: seconds }
-      )
-
-      user.token = token
-      await user.save()
-      return res.status(200).json({ message: 'Usuário cadastrado com sucesso.', user })
+          const response = {
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            token: user.token
+          }
+          return res.status(200).json({ message: 'Usuário cadastrado com sucesso.', response })
+        } catch (err) {
+          const type = err.errors[0]
+          if (type.validatorKey == 'not_unique') {
+            return res.status(400).json({
+              error: true,
+              message: `O nome de usuário "${type.value}" já está em uso.`
+            })
+          } else {
+            return res.status(500)
+          }
+        }
+      })
     } catch (err) {
-      const type = err.errors[0]
-      if (type.validatorKey == 'not_unique') {
-        return res.status(400).json({
-          error: true,
-          message: `O nome de usuário "${type.value}" já está em uso.`
-        })
-      }
       return res.status(500).json({
         error: true,
         message: 'Ocorreu um erro ao tentar realizar a operação, \npor favor tente novamente mais tarde.'
@@ -58,32 +72,36 @@ module.exports = {
         return res.status(404).json({ error: true, message: 'Nome de usuário inválido.' })
       }
       
-      if (user.password != password) {
-        return res.status(401).json({ error: true, message: 'Senha inválida.' })
-      }
+      bcrypt.compare(password, user.password, async (err, result) => {
+        if (err || !result) {
+          return res.status(401).json({
+            error: true,
+            message: 'Senha Inválida.'
+          })
+        }
+        const seconds = 43200
+        const token = jwt.sign(
+          {
+            id: user.id,
+            timestamp: Date.now(),
+            admin: false
+          },
+          secret,
+          { expiresIn: seconds }
+        )
 
-      const seconds = 43200
-      const token = jwt.sign(
-        {
+        user.token = token
+        await user.save()
+
+        const response = {
           id: user.id,
-          timestamp: Date.now(),
-          admin: false
-        },
-        secret,
-        { expiresIn: seconds }
-      )
+          name: user.name,
+          username: user.username,
+          token: user.token
+        }
 
-      user.token = token
-      await user.save()
-
-      const response = {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-        token: user.token
-      }
-
-      return res.status(200).json({ message: 'Autenticado com sucesso.', user: response })
+        return res.status(200).json({ message: 'Autenticado com sucesso.', user: response })
+      })
     } catch (error) {
       return res.status(500).json({
         error: true,
